d68b19bab6fa5141c500c221d568cbd2fe630004
ctoth <ctoth@b35830b6-564f-0410-ac5f-b387e50cb686>
2008-10-08 16:35:25 +0000

    renamed file
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@68 b35830b6-564f-0410-ac5f-b387e50cb686

---
+++ Paradigms/MetaGME/MetaDecorator/Resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by Decorator.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_DECORATOR                   101
+#define IDR_NEWDECORATOR                102
+#define IDC_TEXTEDIT                    201
+#define IDD_INPLACEEDITDIALOG           210
+#define IDB_BITMAP_PROXY                205
+#define IDB_BITMAP_CONSTRAINT           206
+#define IDB_BITMAP_CONSTRAINTFUNC       208
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        211
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         202
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
