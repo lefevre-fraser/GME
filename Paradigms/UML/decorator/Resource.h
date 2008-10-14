17d7a93077ffa8c3b8c6c90a44c067b855ba6865
ctoth <ctoth@b35830b6-564f-0410-ac5f-b387e50cb686>
2008-10-14 23:14:01 +0000

    renamed file
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@77 b35830b6-564f-0410-ac5f-b387e50cb686

---
+++ Paradigms/UML/decorator/Resource.h
@@ -0,0 +1,49 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by UMLDecorator.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_UMLDECORATOR                101
+#define IDR_NEWUMLDECORATOR             102
+
+#define IDB_DEFAULTREFTONULL            159
+#define IDB_REFERENCE                   159
+#define IDB_DEFAULTATOM                 160
+#define IDB_ATOM                        160
+#define IDB_DEFAULTSET                  161
+#define IDB_SET                         161
+#define IDB_DEFAULTNULLREF              166
+#define IDB_DEFAULTATOMPORT             173
+#define IDB_ATOMPORT                    173
+#define IDB_DEFAULTREFPORT              174
+#define IDB_REFERENCEPORT               174
+#define IDB_DEFAULTSETPORT              175
+#define IDB_SETPORT                     175
+#define IDB_DEFAULTMODELPORT            176
+#define IDB_MODELPORT                   176
+#define IDB_ICON_TYPE                   201
+#define IDB_ICON_SUBTYPE                202
+#define IDB_ICON_INSTANCE               203
+#define IDB_ICON_REFERENCE              204
+#define IDB_MODEL                       205
+#define IDB_ICON_INSTANCE_DIS           206
+#define IDB_ICON_REFERENCE_DIS          207
+#define IDB_ICON_SUBTYPE_DIS            208
+#define IDB_ICON_TYPE_DIS               209
+
+#define IDB_BITMAP_COPY                 210
+#define IDB_BITMAP_CONSTRAINT           211
+#define IDB_BITMAP_CDEFINITION          212
+#define IDC_TEXTEDIT                    201
+#define IDD_INPLACEEDITDIALOG           210
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        213
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         202
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
