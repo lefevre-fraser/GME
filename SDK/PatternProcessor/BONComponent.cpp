36e10f4522bd57378f1fff06cc023425abf71871
volgy <volgy@b35830b6-564f-0410-ac5f-b387e50cb686>
2008-12-19 21:50:39 +0000

    Updated pattern processor for the new (wizard based and VS2008 compliant) component frameworks.
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@216 b35830b6-564f-0410-ac5f-b387e50cb686

--- SDK/PatternProcessor/BONComponent.cpp
+++ SDK/PatternProcessor/BONComponent.cpp
@@ -22,7 +22,7 @@
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
 
-#include "Component.h"
+#include "BONComponent.h"
 
 #define PATLINELENGTH 500
 
--- SDK/PatternProcessor/StdAfx.cpp
+++ SDK/PatternProcessor/StdAfx.cpp
@@ -5,5 +5,17 @@
 #include "stdafx.h"
 
 #ifdef _ATL_STATIC_REGISTRY
+
 #include <statreg.h>
-#endif
+
+#if _ATL_VER < 0x0700
+#include <statreg.cpp>
+#endif // _ATL_VER < 0x0700
+
+#endif // _ATL_STATIC_REGISTRY
+
+#if _ATL_VER < 0x0700
+
+#include <atlimpl.cpp>
+
+#endif // _ATL_VER < 0x0700
