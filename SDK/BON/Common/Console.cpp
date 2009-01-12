8a7a72db32f4b9e24b75675d9a39c68f7ee269ba
ctoth <ctoth@b35830b6-564f-0410-ac5f-b387e50cb686>
2009-01-12 17:14:20 +0000

    Console assert (assert if components activated second or more times after each other) fix: consult with Tihamer
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@250 b35830b6-564f-0410-ac5f-b387e50cb686

--- SDK/BON/Common/Console.cpp
+++ SDK/BON/Common/Console.cpp
@@ -5,18 +5,18 @@
 
 namespace GMEConsole
 {
-	CComPtr<IGMEOLEApp> Console::gmeoleapp=0;
+	CComPtr<IGMEOLEApp> Console::gmeoleapp = 0;
 
 	void Console::SetupConsole(CComPtr<IMgaProject> project)
 	{
 		CComPtr<IMgaClient> client;	
 		CComQIPtr<IDispatch> pDispatch;
-		HRESULT s1 = project->GetClientByName(L"GME.Application",&client);
+		HRESULT s1 = project->GetClientByName(L"GME.Application", &client);
 
 		if ((SUCCEEDED(s1)) && (client != 0))
 		{
 			HRESULT s2 = client->get_OLEServer(&pDispatch);
-			if ((SUCCEEDED(s2)) && (pDispatch != 0))
+			if ((SUCCEEDED(s2)) && (pDispatch != 0) && gmeoleapp != 0)
 			{
 				COMTHROW(pDispatch->QueryInterface(&gmeoleapp));
 			}
