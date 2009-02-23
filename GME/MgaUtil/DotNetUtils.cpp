170f12d9c2f10db0c751d79fef207f7026ee103a
ctoth <ctoth@b35830b6-564f-0410-ac5f-b387e50cb686>
2009-02-23 20:52:06 +0000

    MgaUtils CallManagedProcess refactor part 2: getting independent from mscoree.dll by using GetProcAddress.
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@312 b35830b6-564f-0410-ac5f-b387e50cb686

--- GME/MgaUtil/DotNetUtils.cpp
+++ GME/MgaUtil/DotNetUtils.cpp
@@ -7,6 +7,9 @@
 using namespace mscorlib;
 
 
+typedef HRESULT (WINAPI * tCorBindToRuntimeEx)(LPWSTR,LPWSTR,DWORD,REFCLSID,REFIID,LPVOID*);
+
+
 int CDotNetUtils::CallManagedFunction(BSTR assemblyPath, BSTR typeName, BSTR methodName, int mode)
 {
 	return CallManagedFunction(assemblyPath, typeName, methodName, NULL, mode);
@@ -49,102 +52,116 @@ int CDotNetUtils::CallManagedFunction(BSTR assemblyPath, BSTR typeName, BSTR met
 	if(data[0] == 1) {
 		TRACE(".NET Framework v2.0.50727 is installed");
 	} else {
-		AfxMessageBox(".NET Framework v2.0.50727 is NOT installed");
+		AfxMessageBox("CallManagedFunction: .NET Framework v2.0.50727 is NOT installed");
 		return 3;
 	}
 
-	LPWSTR pszVer = L"v2.0.50727";
-	LPWSTR pszFlavor = L"wks";
-	ICorRuntimeHost *pHost = NULL;
+	HMODULE cor = LoadLibrary("mscoree.dll");
+	if (!cor) {
+		AfxMessageBox("CallManagedFunction: couldn't load mscoree.dll (.NET Framework provider dll)");
+		return 3;
+	} else {
+		tCorBindToRuntimeEx pCorBindToRuntimeEx = (tCorBindToRuntimeEx) GetProcAddress(cor, "CorBindToRuntimeEx");
+		if (!pCorBindToRuntimeEx) {
+			AfxMessageBox("CallManagedFunction: couldn't load CorBindToRuntimeEx function entry from mscoree.dll (.NET Framework provider dll)");
+			return 3;
+		} else {
+			LPWSTR pszVer = L"v2.0.50727";
+			LPWSTR pszFlavor = L"wks";
+			ICorRuntimeHost *pHost = NULL;
 
-	HRESULT hr = CorBindToRuntimeEx(pszVer,
-									pszFlavor,
-									STARTUP_LOADER_OPTIMIZATION_SINGLE_DOMAIN | STARTUP_CONCURRENT_GC,
-									CLSID_CorRuntimeHost,
-									IID_ICorRuntimeHost,
-									(void **)&pHost);
+			HRESULT hr = pCorBindToRuntimeEx(pszVer,
+											 pszFlavor,
+											 STARTUP_LOADER_OPTIMIZATION_SINGLE_DOMAIN | STARTUP_CONCURRENT_GC,
+											 CLSID_CorRuntimeHost,
+											 IID_ICorRuntimeHost,
+											 (void **)&pHost);
 
-	if (!SUCCEEDED(hr)) {
-		CString csTemp;
-		if (hr == CLR_E_SHIM_RUNTIMELOAD)
-			csTemp.Format("CallManagedFunction: CorBindToRuntimeEx failed because of mismatching .NET Framework version (CLR_E_SHIM_RUNTIMELOAD).");
-		else
-			csTemp.Format("CallManagedFunction: CorBindToRuntimeEx failed: %ld", hr);
-		AfxMessageBox(csTemp);
-		return 1;
-	}
-
-	TRACE("Loaded version 2.0.50727 of the CLR\n");
-
-	pHost->Start(); // Start the CLR
-
-	//
-	// Get a pointer to the default domain in the CLR
-	//
-	_AppDomainPtr pDefaultDomain = NULL;
-	IUnknownPtr   pAppDomainPunk = NULL;
-
-	hr = pHost->GetDefaultDomain(&pAppDomainPunk);
-	ASSERT(pAppDomainPunk);
-
-	hr = pAppDomainPunk->QueryInterface(__uuidof(_AppDomain), (void**) &pDefaultDomain);
-	ASSERT(pDefaultDomain);
-
-	try
-	{
-		_ObjectHandlePtr pObjectHandle;
-		_ObjectPtr pObject; 
-		_TypePtr pType;
-		SAFEARRAY* psa;
-
-		// Create an instance of a type from an assembly
-		pObjectHandle = pDefaultDomain->CreateInstanceFrom(assemblyPath, typeName);
-
-		variant_t vtobj = pObjectHandle->Unwrap();								// Get an _Object (as variant) from the _ObjectHandle
-		vtobj.pdispVal->QueryInterface(__uuidof(_Object), (void**)&pObject);	// QI the variant for the Object iface
-		pType = pObject->GetType();												// Get the _Type iface
-		psa = SafeArrayCreateVector(VT_VARIANT, 0, 1);							// Create a safearray (0 length)
-		
-		{
-			VARIANT typeNameParam;
-			VariantInit(&typeNameParam);
-			CComPtr<IDispatch> p;
-			if (!param) {
-				typeNameParam.vt = VT_I4;
-				typeNameParam.intVal = mode;
-			} else {
-				typeNameParam.vt = VT_DISPATCH;
-				p = param;
-				typeNameParam.pdispVal = p;
+			if (!SUCCEEDED(hr)) {
+				CString csTemp;
+				if (hr == CLR_E_SHIM_RUNTIMELOAD)
+					csTemp.Format("CallManagedFunction: CorBindToRuntimeEx failed because of mismatching .NET Framework version (CLR_E_SHIM_RUNTIMELOAD).");
+				else
+					csTemp.Format("CallManagedFunction: CorBindToRuntimeEx failed: %ld", hr);
+				AfxMessageBox(csTemp);
+				return 1;
 			}
-			LONG index = 0;
 
-			SafeArrayPutElement(psa, &index, &typeNameParam);
+			TRACE("Loaded version 2.0.50727 of the CLR\n");
+
+			pHost->Start(); // Start the CLR
+
+			//
+			// Get a pointer to the default domain in the CLR
+			//
+			_AppDomainPtr pDefaultDomain = NULL;
+			IUnknownPtr   pAppDomainPunk = NULL;
+
+			hr = pHost->GetDefaultDomain(&pAppDomainPunk);
+			ASSERT(pAppDomainPunk);
+
+			hr = pAppDomainPunk->QueryInterface(__uuidof(_AppDomain), (void**) &pDefaultDomain);
+			ASSERT(pDefaultDomain);
+
+			try
+			{
+				_ObjectHandlePtr pObjectHandle;
+				_ObjectPtr pObject; 
+				_TypePtr pType;
+				SAFEARRAY* psa;
+
+				// Create an instance of a type from an assembly
+				pObjectHandle = pDefaultDomain->CreateInstanceFrom(assemblyPath, typeName);
+
+				variant_t vtobj = pObjectHandle->Unwrap();								// Get an _Object (as variant) from the _ObjectHandle
+				vtobj.pdispVal->QueryInterface(__uuidof(_Object), (void**)&pObject);	// QI the variant for the Object iface
+				pType = pObject->GetType();												// Get the _Type iface
+				psa = SafeArrayCreateVector(VT_VARIANT, 0, 1);							// Create a safearray (0 length)
+				
+				{
+					VARIANT typeNameParam;
+					VariantInit(&typeNameParam);
+					CComPtr<IDispatch> p;
+					if (!param) {
+						typeNameParam.vt = VT_I4;
+						typeNameParam.intVal = mode;
+					} else {
+						typeNameParam.vt = VT_DISPATCH;
+						p = param;
+						typeNameParam.pdispVal = p;
+					}
+					LONG index = 0;
+
+					SafeArrayPutElement(psa, &index, &typeNameParam);
+				}
+
+				//_variant_t retVal = 
+				pType->InvokeMember_3(methodName,										// Invoke "Test" method on pType
+									  BindingFlags_InvokeMethod,
+									  NULL,
+									  vtobj,
+									  psa);
+
+				//if (retVal.vt == VT_I4)
+				//{
+				//	int ret = retVal.intVal;
+				//}
+
+				SafeArrayDestroy(psa);													// Destroy safearray
+			}
+			catch(_com_error& error)
+			{
+				CString csTemp;
+				csTemp.Format("CallManagedFunction exception, ERROR: %s", (_TCHAR*)error.Description());
+				AfxMessageBox(csTemp);
+			}
+
+			pHost->Stop();
+			pHost->Release();
 		}
 
-		//_variant_t retVal = 
-		pType->InvokeMember_3(methodName,										// Invoke "Test" method on pType
-							  BindingFlags_InvokeMethod,
-							  NULL,
-							  vtobj,
-							  psa);
-
-		//if (retVal.vt == VT_I4)
-		//{
-		//	int ret = retVal.intVal;
-		//}
-
-		SafeArrayDestroy(psa);													// Destroy safearray
+		FreeLibrary(cor);
 	}
-	catch(_com_error& error)
-	{
-		CString csTemp;
-		csTemp.Format("CallManagedFunction exception, ERROR: %s", (_TCHAR*)error.Description());
-		AfxMessageBox(csTemp);
-	}
-
-	pHost->Stop();
-	pHost->Release();
 
 	return 0;
 }
