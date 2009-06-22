0b68b03c648592d3506800c6ab8c19153369b9dc
ctoth <ctoth@b35830b6-564f-0410-ac5f-b387e50cb686>
2009-06-22 16:13:32 +0000

    removing old UML decorator - part 3
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@414 b35830b6-564f-0410-ac5f-b387e50cb686

--- Paradigms/UML/decorator/Resource.h
+++ Paradigms/UML/decorator/Resource.h
@@ -4,16 +4,13 @@
 //
 #define IDS_PROJNAME                    100
 #define IDR_UMLDECORATOR                101
-#define IDR_NEWUMLDECORATOR             102
 #define IDB_REFERENCE_SIGN              210
-#define IDB_BITMAP_CONSTRAINT           211
-#define IDB_BITMAP_CDEFINITION          212
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        213
+#define _APS_NEXT_RESOURCE_VALUE        211
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         202
 #define _APS_NEXT_SYMED_VALUE           103
