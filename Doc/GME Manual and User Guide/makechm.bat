94b1ea0ccc02cb8ee14db78e8c63daa59095e8c4
volgy <volgy@b35830b6-564f-0410-ac5f-b387e50cb686>
2010-01-04 18:56:56 +0000

    x64 compatiblity
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@770 b35830b6-564f-0410-ac5f-b387e50cb686

--- Doc/GME Manual and User Guide/makechm.bat
+++ Doc/GME Manual and User Guide/makechm.bat
@@ -5,4 +5,8 @@ SET HTMLHELP="%GME_ROOT%\Doc\GME Manual and User Guide\htmlhelp.xsl"
 
 java -Xmx1500m -cp %XALANCP% org.apache.xalan.xslt.Process -ENTITYRESOLVER org.apache.xml.resolver.tools.CatalogResolver -URIRESOLVER org.apache.xml.resolver.tools.CatalogResolver -in "GME Manual and User Guide.xml" -xsl %HTMLHELP% -out out.test -param chunk.section.depth 8 -param chunk.first.sections 1
 
-"c:\Program Files\HTML Help Workshop\hhc.exe" htmlhelp.hhp
+IF %PROCESSOR_ARCHITECTURE% == AMD64 (
+	"c:\Program Files (x86)\HTML Help Workshop\hhc.exe" htmlhelp.hhp
+) ELSE (
+	"c:\Program Files\HTML Help Workshop\hhc.exe" htmlhelp.hhp
+)
