053e1296d981d3e0e29a95a2004d33cc073b7d0b
volgy <volgy@b35830b6-564f-0410-ac5f-b387e50cb686>
2010-01-11 23:11:16 +0000

    Initial version of a simple validator (ascii chars only)
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@792 b35830b6-564f-0410-ac5f-b387e50cb686

---
+++ Tools/XMETidy/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by XMETidy.rc
+//
+#define IDC_MYICON                      2
+#define IDD_XMETIDY_DIALOG              102
+#define IDM_ABOUT                       104
+#define IDI_XMETIDY                     107
+#define IDC_XMETIDY                     109
+#define IDC_EDIT1                       1000
+#define IDC_BUTTON1                     1001
+#define IDC_BUTTON2                     1002
+#define IDC_LIST1                       1003
+#define IDC_EDIT2                       1004
+#define IDC_BUTTON3                     1005
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
