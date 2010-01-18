65c53874d9c1900aa325b0b9006502b1da39e02f
volgy <volgy@b35830b6-564f-0410-ac5f-b387e50cb686>
2010-01-18 18:45:56 +0000

    A little tweak on this unfinished script.
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@803 b35830b6-564f-0410-ac5f-b387e50cb686

--- Install/symbols.cmd
+++ Install/symbols.cmd
@@ -1,3 +1,3 @@
-set VERSION=10.1.11
+set VERSION=9.12.29
 set DBGTOOLS=C:\Program Files\Debugging Tools for Windows (x64)
-"%DBGTOOLS%\symstore.exe" add /f %GME_ROOT%/Install/symbols /s \\atlantis\project\GME\symbols /r /t GME /v %VERSION% /c "GME Release %VERSION% symbols added" /compress
+"%DBGTOOLS%\symstore.exe" add /f %GME_ROOT%/Install/GME-%VERSION%-symbols /s \\atlantis\project\GME\symbols /r /t GME /v %VERSION% /c "GME Release %VERSION% symbols added" /compress
