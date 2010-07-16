aec55366eb1b6c04b4b7d54561d99f4cf187fe5e
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2010-07-16 21:51:52 +0000

    Nonfunctional .NET component wizard
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@904 b35830b6-564f-0410-ac5f-b387e50cb686

---
+++ SDK/DotNet/Wizard/Templates/1033/Component.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.IO;
+
+
+using GME.CSharp;
+using GmeLib;
+
+
+namespace GME.Interpreter
+{
+    [Guid(ComponentConfig.guid),
+    ProgId(ComponentConfig.progID),
+    ClassInterface(ClassInterfaceType.AutoDual)]
+    [ComVisible(true)]
+    public class [!output COMPONENT_NAME] : GmeLib.IMgaComponentEx, GmeLib.IMgaVersionInfo
+    {
+        public void InvokeEx2(IMgaProject project, IMgaFCO currentobj, IMgaFCOs selectedobjs, int param)
+        {
+            IGMEOLEApp gme = null;
+            try
+            {
+                gme = (IGMEOLEApp)project.GetClientByName("GME.Application").OLEServer;
+            }
+            catch { }
+            gme.ConsoleMessage("Interpreting...", msgtype_enum.MSG_INFO);
+
+            GmeLib.IMgaTerritory territory = project.BeginTransactionInNewTerr(transactiontype_enum.TRANSACTION_READ_ONLY);
+            try
+            {
+            	// TODO: interpreter code goes here
+                IMgaFolder rootFolder = project.RootFolder;
+                gme.ConsoleMessage(rootFolder.Name, msgtype_enum.MSG_INFO);
+            }
+            finally
+            {
+                try
+                {
+                    project.AbortTransaction();
+                }
+                catch { }
+                try
+                {
+                    territory.Destroy();
+                }
+                catch { }
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(project);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(territory);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(selectedobjs);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(gme);
+            }
+        }
+
+        #region IMgaComponentEx Members
+
+        public void InvokeEx(IMgaProject project, IMgaFCO currentobj, IMgaFCOs selectedobjs, int param)
+        {
+            try
+            {
+                InvokeEx2(project, currentobj, selectedobjs, param);
+            }
+            finally
+            {
+                project = null;
+                currentobj = null;
+                selectedobjs = null;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        }
+
+        public string ComponentName
+        {
+            get { return GetType().Name; }
+        }
+
+        public string ComponentProgID
+        {
+            get
+            {
+                RegistrationServices regService = new RegistrationServices();
+                return regService.GetProgIdForType(GetType());
+            }
+        }
+
+        public componenttype_enum ComponentType
+        {
+            get { return GmeLib.componenttype_enum.COMPONENTTYPE_INTERPRETER; }
+        }
+
+        public void Enable(bool newval)
+        {
+        }
+
+        public void Initialize(IMgaProject p)
+        {
+        }
+
+        protected bool interactiveMode = false;
+        public bool InteractiveMode
+        {
+            get
+            {
+                return interactiveMode;
+            }
+            set
+            {
+                interactiveMode = value;
+            }
+        }
+
+        public void Invoke(IMgaProject Project, IMgaFCOs selectedobjs, int param)
+        {
+            InvokeEx(Project, null, selectedobjs, 0);
+        }
+
+        public void ObjectsInvokeEx(IMgaProject Project, IMgaObject currentobj, IMgaObjects selectedobjs, int param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Paradigm
+        {
+            get { return ComponentConfig.paradigmName; }
+        }
+
+        public object get_ComponentParameter(string Name)
+        {
+            if (Name == "type")
+                return "csharp";
+
+            if (Name == "path")
+                return GetType().Assembly.Location;
+
+            if (Name == "fullname")
+                return GetType().FullName;
+
+            return null;
+        }
+
+        public void set_ComponentParameter(string Name, object pVal)
+        {
+        }
+
+        #endregion
+
+        #region IMgaVersionInfo Members
+
+        public MgaInterfaceVersion_enum Version
+        {
+            get { return GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current; }
+        }
+
+        #endregion
+    
+        [ComRegisterFunctionAttribute]
+        public static void GMERegister(Type t)
+        {
+            Registrar.RegisterComponentsInGMERegistry();
+          
+        }
+
+        [ComUnregisterFunctionAttribute]
+        public static void GMEUnRegister(Type t)
+        {
+            Registrar.UnRegisterComponentsInGMERegistry();
+
+        }
+    }
+}
---
+++ SDK/DotNet/Wizard/Templates/1033/Registrar.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+// new GME registrar
+namespace GME.Interpreter
+{
+    [ComVisible(false)]
+    class RegistrationException : ApplicationException
+    {
+        public RegistrationException(string message) : base(message) { }
+    }
+
+    [ComVisible(false)]
+    class Registrar
+    {
+        private Registrar()
+        {
+        }
+
+        public static void RegisterComponentsInGMERegistry()
+        {
+            MGAUTILLib.MgaRegistrar registrar = new MGAUTILLib.MgaRegistrar();
+            if ((int)GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current != (int)((GmeLib.IMgaVersionInfo)registrar).Version)
+            {
+                throw new RegistrationException("MgaInterfaceVersion mismatch: this assembly is using " +
+                    (int)GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current +
+                    " but the GME interface version is " + (int)((GmeLib.IMgaVersionInfo)registrar).Version +
+                    "\n\nPlease install a compatible GME version or update the interop dlls.");
+            }
+
+            registrar.RegisterComponent(ComponentConfig.progID, ComponentConfig.componentType, ComponentConfig.componentName, ComponentConfig.registrationMode);
+            registrar.set_ComponentExtraInfo(ComponentConfig.registrationMode, ComponentConfig.progID, "Icon", ComponentConfig.iconId);
+
+            if (!ComponentConfig.paradigmName.Equals("*"))
+            {
+                foreach (String paradigm in ComponentConfig.paradigmName.Split(','))
+                {
+                    registrar.Associate(ComponentConfig.progID, paradigm, ComponentConfig.registrationMode);
+                }
+            }
+        }
+
+        public static void UnregisterComponentsInGMERegistry()
+        {
+            MGAUTILLib.MgaRegistrar registrar = new MGAUTILLib.MgaRegistrar();
+            if ((int)GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current != (int)((GmeLib.IMgaVersionInfo)registrar).Version)
+            {
+                throw new RegistrationException("MgaInterfaceVersion mismatch: this assembly is using " +
+                    (int)GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current +
+                    " but the GME interface version is " + (int)((GmeLib.IMgaVersionInfo)registrar).Version +
+                    "\n\nPlease install a compatible GME version or update the interop dlls.");
+            }
+
+            registrar.UnregisterComponent(ComponentConfig.progID, ComponentConfig.registrationMode);
+        }
+
+        public void DLLRegisterServer(int regMode)
+        {
+            //register dll 
+            //same as:
+            //post build event: C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\regasm.exe $(TargetPath) /codebase
+
+            RegistrationServices regAsm = new RegistrationServices();
+            bool bResult = regAsm.RegisterAssembly(ComponentConfig.typeToRegister.Assembly, AssemblyRegistrationFlags.SetCodeBase);
+        }
+    }
+}
+
+#region Old-GME style registrar
+namespace GME.CSharp
+{
+    [ComVisible(false)]
+    public class Registrar
+    {
+        public Registrar()
+        {
+        }
+
+        MGAUTILLib.regaccessmode_enum regacc_translate(int x)
+        {
+            return (MGAUTILLib.regaccessmode_enum)x;
+        }
+
+        public void DLLRegisterServer(int regMode)
+        {
+            //register dll 
+            //same as:
+            //post build event: C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727\regasm.exe $(TargetPath) /codebase
+
+            RegistrationServices regAsm = new RegistrationServices();
+            bool bResult = regAsm.RegisterAssembly(
+                ComponentConfig.typeToRegister.Assembly,
+                AssemblyRegistrationFlags.SetCodeBase);
+
+            MGAUTILLib.MgaRegistrar reg = new MGAUTILLib.MgaRegistrar();
+            if ((int)GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current != (int)((GmeLib.IMgaVersionInfo)reg).Version)
+            {
+                throw new Exception("MgaInterfaceVersion mismatch: this assembly is using " +
+                    (int)GmeLib.MgaInterfaceVersion_enum.MgaInterfaceVersion_Current +
+                    " but the GME interface version is " + (int)((GmeLib.IMgaVersionInfo)reg).Version +
+                    "\n\nPlease install a compatible GME version or update the interop dlls.");
+            }
+            reg.RegisterComponent(
+                ComponentConfig.progID,
+                MGALib.componenttype_enum.COMPONENTTYPE_INTERPRETER,
+                ComponentConfig.componentName,
+                regacc_translate(regMode));
+
+            reg.set_ComponentExtraInfo(regacc_translate(regMode), ComponentConfig.progID, "Icon", ComponentConfig.iconId);
+
+            if (!ComponentConfig.paradigmName.Equals("*"))
+            {
+                reg.Associate(ComponentConfig.progID, ComponentConfig.paradigmName, (MGAUTILLib.regaccessmode_enum)regMode);
+            }
+
+        }
+    }
+}
+#endregion
