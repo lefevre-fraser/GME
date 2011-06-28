66b89f83c7033e5e535b688afa7ba80963f76edd
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2011-06-28 22:03:31 +0000

    Import tlbs so build doesnt depend on dlls
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1392 b35830b6-564f-0410-ac5f-b387e50cb686

--- GME/PartBrowser/stdafx.cpp
+++ GME/PartBrowser/stdafx.cpp
@@ -4,8 +4,8 @@
 
 #include "stdafx.h"
 
-#import "Meta.dll" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
-#import "Mga.dll" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
-#import "GME.exe" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
-#import "MgaUtil.dll" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
+#import "MetaLib.tlb" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
+#import "MgaLib.tlb" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
+#import "GMELib.tlb" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
+#import "MgaUtilLib.tlb" implementation_only auto_search no_namespace no_search_namespace raw_method_prefix("") high_method_prefix("__")
 
