5885ba49cbea1c340214340405c93d2e443dd0bb
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2011-09-29 19:35:27 +0000

    Test Version. typo
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1559 b35830b6-564f-0410-ac5f-b387e50cb686

--- Tests/GPyUnit/test_gmeoleapp.py
+++ Tests/GPyUnit/test_gmeoleapp.py
@@ -4,8 +4,9 @@ class TestGMEOLEApp(unittest.TestCase):
     def test_Exit(self):
         import win32com.client
         gme = win32com.client.DispatchEx("GME.Application")
+        gme.Version
         gme.Exit()
-        # under _DEBUG, GME.exe does not exist until Release()ed
+        # under _DEBUG, GME.exe does not exit until Release()ed
         del(gme)
 
 if __name__ == "__main__":
