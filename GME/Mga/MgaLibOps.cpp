lefevre-fraser/GME

commit a38ef15b9ebfb8c337cfdd72ea8545ef3d33afc2

Store regnodes in a hashmap. Uses an order of magnitude less memory
git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1696 b35830b6-564f-0410-ac5f-b387e50cb686

GME/Mga/MgaLibOps.cpp
@@ -280,9 +280,6 @@ void LibImgHelper::DoExtTreeCopy::operator ()( const CoreObj& orig, CoreObj& nob
 	metaid_type s;
 	COMTHROW( m_mgaproject->dataproject->CreateObject(s = orig.GetMetaID(), &nobj.ComPtr()));
 
-	if( s != DTID_REGNODE )		// speedup, no references to regnodes
-		m_fixup.identify(orig, nobj);
-
 	bool skip_this = false;
 	if(s >= DTID_MODEL && s <= DTID_FOLDER) {
 		setcheck( m_mgaproject, nobj, CHK_NEW);
@@ -701,7 +698,6 @@ void redo_derivs(CMgaProject *mgaproject, CoreObj &oldnode, CoreObj &newnode, bo
 	if(oldnode.IsContainer()) {
 		newnode[ATTRID_LASTRELID] = oldnode[ATTRID_LASTRELID];
 	}
-	steal(oldnode, newnode, ATTRID_REGNOWNER);
 	steal(oldnode, newnode, ATTRID_CONSTROWNER);
 
 	if(oldnode.IsFCO()) {

GME/Core/Core.vcxproj
@@ -440,6 +440,7 @@
     <ClCompile Include="Core.cpp" />
     <ClCompile Include="CoreAttribute.cpp" />
     <ClCompile Include="CoreBinFile.cpp" />
+    <ClCompile Include="CoreDictionaryAttributeValue.cpp" />
     <ClCompile Include="CoreMetaAttribute.cpp" />
     <ClCompile Include="CoreMetaObject.cpp" />
     <ClCompile Include="CoreMetaProject.cpp" />
@@ -497,6 +498,7 @@
   <ItemGroup>
     <ClInclude Include="CoreAttribute.h" />
     <ClInclude Include="CoreBinFile.h" />
+    <ClInclude Include="CoreDictionaryAttributeValue.h" />
     <ClInclude Include="CoreMetaAttribute.h" />
     <ClInclude Include="CoreMetaObject.h" />
     <ClInclude Include="CoreMetaProject.h" />

GME/Core/Core.vcxproj.filters
@@ -58,6 +58,9 @@
     <ClCompile Include="StdAfx.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
+    <ClCompile Include="CoreDictionaryAttributeValue.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <None Include="CoreBinFile.rgs">
@@ -134,6 +137,9 @@
       <Filter>Header Files</Filter>
     </ClInclude>
     <ClInclude Include="targetver.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="CoreDictionaryAttributeValue.h">
       <Filter>Header Files</Filter>
     </ClInclude>
   </ItemGroup>

GME/Core/CoreAttribute.cpp
@@ -45,6 +45,7 @@ typedef CCoreDataAttribute<CCoreDataAttrBase<long>, VALTYPE_LONG> CCoreLongAttri
 typedef CCoreDataAttribute<CCoreDataAttrBase<CComBstrObj>, VALTYPE_STRING> CCoreStringAttribute;
 typedef CCoreDataAttribute<CCoreDataAttrBase<bindata>, VALTYPE_BINARY> CCoreBinaryAttribute;
 typedef CCoreDataAttribute<CCoreDataAttrBase<double>, VALTYPE_REAL> CCoreRealAttribute;
+typedef CCoreDataAttribute<CCoreDataAttrBase<CComPtr<CCoreDictionaryAttributeValue>>, VALTYPE_DICT> CCoreDictAttribute;
 
 void CCoreAttribute::Create(CCoreObject *object, CCoreMetaAttribute *metaattribute)
 {
@@ -80,6 +81,17 @@ void CCoreAttribute::Create(CCoreObject *object, CCoreMetaAttribute *metaattribu
 	case VALTYPE_LONG:
 		{
 			typedef CComPartObject< CCoreLongAttribute > COMTYPE;
+
+			COMTYPE *p = NULL;
+			COMTHROW( COMTYPE::CreateInstance(CastToUnknown(object), &p) );
+			attribute = p;
+
+			break;
+		}
+
+		case VALTYPE_DICT:
+		{
+			typedef CComPartObject< CCoreDictAttribute > COMTYPE;
 
 			COMTYPE *p = NULL;
 			COMTHROW( COMTYPE::CreateInstance(CastToUnknown(object), &p) );
@@ -819,13 +831,36 @@ inline void CCoreDataAttrBase<DATA>::ChangeFrontValue(VARIANT &v)
 	CopyTo(v, values.front());
 }
 
+template<>
+inline void CCoreDataAttrBase<CComPtr<CCoreDictionaryAttributeValue>>::ChangeFrontValue(VARIANT &v)
+{
+	ASSERT( values.size() >= 2 );
+}
+
+
+template<class DATA>
+DATA CCoreDataAttrBase<DATA>::CreateValue()
+{
+	return value_type();
+}
+
+template<>
+CComPtr<CCoreDictionaryAttributeValue> CCoreDataAttrBase<CComPtr<CCoreDictionaryAttributeValue>>::CreateValue()
+{
+	CCoreDictionaryAttributeValue *val = NULL;
+	typedef CComObject< CCoreDictionaryAttributeValue > COMTYPE;
+	// FIXME: is this necessary?
+	COMTHROW( COMTYPE::CreateInstance((COMTYPE **)&val) );
+	return CComPtr<CCoreDictionaryAttributeValue>(val);
+}
+
 template<class DATA>
 void CCoreDataAttrBase<DATA>::InsertFrontValue(VARIANT &v)
 {
 	if( limited_size(values, 2) == 1 )
 	{
 		LockSelfTry();
-		values.push_front(value_type());
+		values.push_front(CreateValue());
 		try
 		{
 			CopyTo(v, values.front());
@@ -840,7 +875,7 @@ void CCoreDataAttrBase<DATA>::InsertFrontValue(VARIANT &v)
 	}
 	else
 	{
-		values.push_front(value_type());
+		values.push_front(CreateValue());
 		try
 		{
 			CopyTo(v, values.front());

GME/Core/CoreAttribute.h
@@ -7,6 +7,7 @@
 #include "CoreTransactionItem.h"
 #include "CoreMetaAttribute.h"
 #include "CoreObject.h"
+#include "CoreDictionaryAttributeValue.h"
 
 #include <list>
 #include <set>
@@ -252,6 +253,7 @@ public:
 
 public:
 	void ChangeFrontValue(VARIANT &v);
+	DATA CreateValue();
 	void InsertFrontValue(VARIANT &v);
 
 	void SpliceValue(values_iterator before, values_iterator pos) NOTHROW;

GME/Core/CoreBinFile.cpp
@@ -44,6 +44,9 @@ BinAttrBase *BinAttrBase::Create(BinAttrBase& attr, valtype_type valtype)
 		binattr = new ((void*)(&attr)) BinAttr<VALTYPE_COLLECTION>;
 		break;
 
+	case VALTYPE_DICT:
+		binattr = new ((void*)(&attr)) BinAttr<VALTYPE_DICT>;
+		break;
 	case VALTYPE_REAL:
 		binattr = new ((void*)(&attr)) BinAttr<VALTYPE_REAL>;
 		break;
@@ -79,7 +82,70 @@ void getMeAGuid( long *p_l1, long *p_l2, long *p_l3, long *p_l4)
 	*p_l4 = (((((t_guid.Data4[4] << 8) + t_guid.Data4[5]) << 8) + t_guid.Data4[6]) << 8) + t_guid.Data4[7];
 }
 
-bool BinObject::HasGuidAndStatAttributes( bool* p_guidFound, bool* p_statusFound)
+void WalkRegistry(CCoreDictionaryAttributeValue::map_type& map, CComBSTR& path, CCoreBinFile* p_bf, BinObject& node)
+{
+	binattrs_iterator i = node.binattrs.begin();
+	binattrs_iterator e = node.binattrs.end();
+	while( i != e)
+	{
+		if( i->attrid == ATTRID_REGNOWNER + ATTRID_COLLECTION) {
+			// Copy, since we're going to remove from it
+			std::vector<objects_iterator> a = ((BinAttr<VALTYPE_COLLECTION>*)(void*)&(*i))->a;
+			for (auto it = a.begin(); it != a.end(); it++)
+			{
+				objects_type::iterator regnode = p_bf->objects.find((*it)->first);
+				if (regnode == p_bf->objects.end())
+					throw E_BINFILE;
+
+				CComVariant name;
+				regnode->second.Find(ATTRID_NAME)->Get(p_bf, &name);
+#define RFLAG_HASVALUE 1
+#define RFLAG_OPAQUE 2
+				CComVariant flag;
+				regnode->second.Find(ATTRID_REGFLAGS)->Get(p_bf, &flag);
+
+				CComBSTR newPath(path);
+				if (path != L"")
+					newPath += L"/";
+				newPath += name.bstrVal;
+				if (flag.intVal & RFLAG_HASVALUE) {
+					CComVariant value;
+					regnode->second.Find(ATTRID_REGNODEVALUE)->Get(p_bf, &value);
+					map[newPath] = CComBSTR(value.bstrVal);
+				}
+				WalkRegistry(map, newPath, p_bf, regnode->second);
+				p_bf->opened_object = regnode;
+				regnode->second.Find(ATTRID_REGNOWNER)->Set(p_bf, CComVariant());
+			}
+			return;
+		}
+		++i;
+	}
+}
+
+void BinObject::UpgradeRegistryIfNecessary(CCoreBinFile* p_bf)
+{
+	binattrs_iterator i = binattrs.begin();
+	binattrs_iterator e = binattrs.end();
+	while( i != e)
+	{
+		if( i->attrid == ATTRID_REGNOWNER + ATTRID_COLLECTION) {
+			CCoreDictionaryAttributeValue::map_type map;
+			
+			WalkRegistry(map, CComBSTR(), p_bf, *this);
+
+			BinAttrBase::Create(*i, VALTYPE_DICT);
+			i->attrid = ATTRID_REGNODE;
+			CComVariant dict;
+			i->Get(p_bf, &dict);
+			((CCoreDictionaryAttributeValue*)dict.pdispVal)->m_dict = map;
+			return;
+		}
+		++i;
+	}
+}
+
+bool BinObject::HasGuidAndStatAttributes( bool* p_guidFound, bool* p_statusFound, bool* p_oldRegFound)
 {
 	int a1( 0), a2( 0), a3( 0), a4( 0);
 
@@ -94,6 +160,8 @@ bool BinObject::HasGuidAndStatAttributes( bool* p_guidFound, bool* p_statusFound
 			case ATTRID_GUID3: ++a3;break;
 			case ATTRID_GUID4: ++a4;break;
 			case ATTRID_FILESTATUS: *p_statusFound = true; break;
+			case ATTRID_REGNOWNER + ATTRID_COLLECTION:
+				*p_oldRegFound = true; break;
 		};
 
 		++i;
@@ -232,6 +300,7 @@ void BinObject::Read(CCoreBinFile *binfile)
 			{ int len; binfile->read(len); binfile->cifs += len; } // FIXME maybe cifs > cifs_eof
 			break;
 
+		case VALTYPE_DICT:
 		case VALTYPE_BINARY:
 			{ int len; binfile->read(len); binfile->cifs += len; } // FIXME maybe cifs > cifs_eof
 			break;
@@ -965,6 +1034,7 @@ void CCoreBinFile::LoadProject()
 		HR_THROW(E_PROJECT_MISMATCH);
 
 	ASSERT( resolvelist.empty() );
+	bool oldReg_found = false;
 
 	for(;;)
 	{
@@ -1001,7 +1071,7 @@ void CCoreBinFile::LoadProject()
 		{
 			bool stat_found( false), guid_found( false);
 
-			opened_object->second.HasGuidAndStatAttributes( &guid_found, &stat_found);
+			opened_object->second.HasGuidAndStatAttributes( &guid_found, &stat_found, &oldReg_found);
 
 			if( !guid_found) // we will create guid attributes for it
 				opened_object->second.CreateGuidAttributes( this);
@@ -1036,6 +1106,23 @@ void CCoreBinFile::LoadProject()
 
 	isEmpty = true;
 	resolvelist.clear();
+
+	if (oldReg_found) {
+		for (auto it = objects.begin(); it != objects.end(); it++)
+		{
+			if (it->first.metaid >= DTID_MODEL && it->first.metaid <= DTID_FOLDER)	// 101 .. 106
+			{
+				it->second.UpgradeRegistryIfNecessary(this);
+			}
+		}
+		for (auto it = objects.begin(); it != objects.end(); )
+		{
+			if (it->first.metaid == DTID_REGNODE)
+				objects.erase(it++);
+			else
+				it++;
+		}
+	}
 
 	ofs.clear();
 	  // FIXME: set read_only correctly

GME/Core/CoreBinFile.h
@@ -2,6 +2,8 @@
 #ifndef MGA_COREBINFILE_H
 #define MGA_COREBINFILE_H
 
+#include "CoreDictionaryAttributeValue.h"
+
 #include <fstream>//fstream.h
 #include <list>//slist
 #include <map>
@@ -143,9 +145,10 @@ public:
 	binattrs_type binattrs;
 	bool deleted;
 
-	bool HasGuidAndStatAttributes( bool* p_guidFound, bool* p_statusFound);
+	bool HasGuidAndStatAttributes( bool* p_guidFound, bool* p_statusFound, bool* p_oldRegFound);
 	void CreateGuidAttributes( CCoreBinFile* p_bf);
 	void CreateStatusAttribute( CCoreBinFile* p_bf);
+	void UpgradeRegistryIfNecessary(CCoreBinFile* binFile);
 
 	BinAttrBase *Find(attrid_type attrid)
 	{
@@ -266,6 +269,9 @@ public:
 	void write(const CComBstrObj &a);
 	void write(const bindata &a);
 	void write(const unsigned char* a, int len);
+	void write(const wchar_t* a, int len) {
+		write((const unsigned char*)a, len * sizeof(wchar_t));
+	}
 	void writestring(const char* pos);
 // ------- Attribute
 
@@ -519,6 +525,107 @@ public:
 	BinAttr(BinAttr<VALTYPE_BINARY>&& that) : BinAttrBase(that.attrid), data(that.data), need_free(that.need_free) { that.need_free = false; }
 	virtual void move(BinAttrUnion&& dest) {
 		new (&dest) BinAttr<VALTYPE_BINARY>(std::move(*this));
+	}
+};
+
+// --------------------------- BinAttr<VALTYPE_DICT>
+
+template<>
+class BinAttr<VALTYPE_DICT> : public BinAttrBase
+{
+public:
+	BinAttr() : data(0) {
+		CCoreDictionaryAttributeValue *val = NULL;
+		typedef CComObject< CCoreDictionaryAttributeValue > COMTYPE;
+		HRESULT hr = COMTYPE::CreateInstance((COMTYPE **)&val);
+		COMTHROW(hr);
+		dict = val;
+	}
+	virtual ~BinAttr() { }
+
+	char* data;
+	// memcpy: if lazy read, data is not guaranteed to be properly aligned for int*
+	int read_len(char*& offset) const { int ret; memcpy(&ret, offset, sizeof(int)); offset += sizeof(int); return ret; }
+
+	CComPtr<ICoreDictionaryAttributeValue> dict;
+
+	virtual valtype_type GetValType() const NOTHROW { return VALTYPE_DICT; }
+	virtual void Set(CCoreBinFile *binfile, VARIANT v)
+	{
+		ASSERT( binfile != NULL );
+		ASSERT(v.vt = VT_DISPATCH);
+		binfile->modified = true;
+		dict = 0;
+		v.pdispVal->QueryInterface(&dict);
+	}
+
+	virtual void Get(CCoreBinFile *binfile, VARIANT *p) const {
+		if (dict == 0) {
+			// lazy read
+			CCoreDictionaryAttributeValue* val = NULL;
+			typedef CComObject< CCoreDictionaryAttributeValue > COMTYPE;
+			HRESULT hr = COMTYPE::CreateInstance((COMTYPE **)&val);
+			COMTHROW(hr);
+
+			char* data = this->data;
+			int size = read_len(data);
+			while (data < this->data + size)
+			{
+				int keysize = read_len(data);
+				CComBSTR key(keysize / sizeof(wchar_t));
+				memcpy(key.m_str, data, keysize);
+				data += keysize;
+				int valuesize = read_len(data);
+				CComBSTR value(valuesize / sizeof(wchar_t));
+				memcpy(value.m_str, data, valuesize);
+				data += valuesize;
+				val->m_dict.emplace(
+					std::unordered_map<CComBSTR, CComBSTR, CComBSTR_Length>::value_type(std::move(key), std::move(value)));
+			}
+
+			BinAttr<VALTYPE_DICT>* _this = const_cast<BinAttr<VALTYPE_DICT>*>(this);
+			_this->dict = val;
+			_this->data = 0;
+		}
+		CComVariant ret = dict;
+		COMTHROW(ret.Detach(p));
+	}
+	virtual void Write(CCoreBinFile *binfile) const {
+		int size = 0;
+
+		if (dict == NULL)
+		{
+			// need to read before write
+			// TODO: could just blit it
+			CComVariant p;
+			Get(binfile, &p);
+		}
+
+		const CCoreDictionaryAttributeValue* cdict = (const CCoreDictionaryAttributeValue*)(const ICoreDictionaryAttributeValue*)dict;
+		for (auto it = cdict->m_dict.begin(); it != cdict->m_dict.end(); it++) {
+			size += sizeof(int);
+			size += it->first.Length() * sizeof(wchar_t);
+			size += sizeof(int);
+			size += it->second.Length() * sizeof(wchar_t);
+		}
+		binfile->write(size);
+
+		for (auto it = cdict->m_dict.begin(); it != cdict->m_dict.end(); it++) {
+			// binfile->write((int)it->first.Length());
+			binfile->write(it->first, it->first.Length());
+			// binfile->write((int)it->second.Length());
+			binfile->write(it->second, it->second.Length());
+		}
+	}
+	virtual void Read(CCoreBinFile *binfile) { 
+		dict = 0;
+		data = (char*)binfile->cifs;
+		int len = read_len(binfile->cifs);
+		binfile->cifs += len;
+	}
+	BinAttr(BinAttr<VALTYPE_DICT>&& that) : BinAttrBase(that.attrid), data(that.data), dict(std::move(that.dict)) { }
+	virtual void move(BinAttrUnion&& dest) {
+		new (&dest) BinAttr<VALTYPE_DICT>(std::move(*this));
 	}
 };
 

GME/Core/CoreDictionaryAttributeValue.cpp
@@ -1,0 +1,29 @@
+#include "StdAfx.h"
+
+#include "CoreDictionaryAttributeValue.h"
+
+STDMETHODIMP CCoreDictionaryAttributeValue::put_Value(BSTR Key, BSTR Value)
+{
+COMTRY {
+	m_dict[CComBSTR(Key)] = CComBSTR(Value);
+} COMCATCH(;)
+}
+STDMETHODIMP CCoreDictionaryAttributeValue::get_Value(BSTR Key, BSTR* Value)
+{
+COMTRY {
+	CComBSTR _key;
+	_key.Attach(Key);
+	auto ent = m_dict.find(_key);
+	_key.Detach();
+	if (ent == m_dict.end())
+		return E_NOTFOUND;
+	*Value = CComBSTR(ent->second).Detach();
+} COMCATCH(;)
+}
+
+STDMETHODIMP CCoreDictionaryAttributeValue::get_Keys(VARIANT* Keys)
+{
+COMTRY {
+	return E_NOTIMPL;
+} COMCATCH(;)
+}

commit 29fbbb85ea6281ffc08f33120fe56ab3e4cb149f

Fix library attaching regression from r1696
git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1699 b35830b6-564f-0410-ac5f-b387e50cb686

GME/Mga/MgaLibOps.cpp
@@ -280,6 +280,9 @@ void LibImgHelper::DoExtTreeCopy::operator ()( const CoreObj& orig, CoreObj& nob
 	metaid_type s;
 	COMTHROW( m_mgaproject->dataproject->CreateObject(s = orig.GetMetaID(), &nobj.ComPtr()));
 
+	m_fixup.identify(orig, nobj);
+
+
 	bool skip_this = false;
 	if(s >= DTID_MODEL && s <= DTID_FOLDER) {
 		setcheck( m_mgaproject, nobj, CHK_NEW);