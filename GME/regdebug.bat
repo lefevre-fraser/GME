a1dabd1ed3b472dbc4a1985d361ba9330b199f6f
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2012-01-04 20:34:34 +0000

    Support regdebug.bat x64
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1765 b35830b6-564f-0410-ac5f-b387e50cb686

--- GME/regdebug.bat
+++ GME/regdebug.bat
@@ -6,77 +6,77 @@ REM	Please consult with Peter before editing this file
 REM 
 
 echo Core.dll:
-regsvr32 /s "%~dp0Debug\Core.dll"
+regsvr32 /s "%~dp0%1\Debug\Core.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo Meta.dll:
-regsvr32 /s "%~dp0Debug\Meta.dll"
+regsvr32 /s "%~dp0%1\Debug\Meta.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo Mga.dll:
-regsvr32 /s "%~dp0Debug\Mga.dll"
+regsvr32 /s "%~dp0%1\Debug\Mga.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo MgaUtil.dll:
-regsvr32 /s "%~dp0Debug\MgaUtil.dll"
+regsvr32 /s "%~dp0%1\Debug\MgaUtil.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo Parser.dll:
-regsvr32 /s "%~dp0Debug\Parser.dll"
+regsvr32 /s "%~dp0%1\Debug\Parser.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo ConstraintManager.dll:
-regsvr32 /s "%~dp0Debug\ConstraintManager.dll"
+regsvr32 /s "%~dp0%1\Debug\ConstraintManager.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo GMEActiveBrowser.ocx:
-regsvr32 /s "%~dp0Debug\GMEActiveBrowser.ocx"
+regsvr32 /s "%~dp0%1\Debug\GMEActiveBrowser.ocx"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo MgaDecorators.dll:
-regsvr32 /s "%~dp0Debug\MgaDecorators.dll"
+regsvr32 /s "%~dp0%1\Debug\MgaDecorators.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo Annotator.dll:
-regsvr32 /s "%~dp0Debug\Annotator.dll"
+regsvr32 /s "%~dp0%1\Debug\Annotator.dll"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo ObjectInspector.ocx:
-regsvr32 /s "%~dp0Debug\ObjectInspector.ocx"
+regsvr32 /s "%~dp0%1\Debug\ObjectInspector.ocx"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo Search.ocx:
-regsvr32 /s "%~dp0Debug\Search.ocx"
+regsvr32 /s "%~dp0%1\Debug\Search.ocx"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo Console.ocx:
-regsvr32 /s "%~dp0Debug\Console.ocx"
+regsvr32 /s "%~dp0%1\Debug\Console.ocx"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo XmlBackEnd.dll:
-regsvr32 /s "%~dp0Debug\XmlBackEnd.dll"
+if %1x NEQ x64x ( regsvr32 /s "%~dp0%1\Debug\XmlBackEnd.dll" )
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo PartBrowser.ocx:
-regsvr32 /s "%~dp0Debug\PartBrowser.ocx"
+regsvr32 /s "%~dp0%1\Debug\PartBrowser.ocx"
 if errorlevel 1 goto errorlabel
 echo    OK
 
 echo PanningView.ocx:
-regsvr32 /s "%~dp0Debug\PanningView.ocx"
+regsvr32 /s "%~dp0%1\Debug\PanningView.ocx"
 if errorlevel 1 goto errorlabel
 echo    OK
 
