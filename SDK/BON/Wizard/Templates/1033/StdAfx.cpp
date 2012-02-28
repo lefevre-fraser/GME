58aff5c5b43c85e994ae4c4c3ea1440cf20a1b4b
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2012-02-28 20:33:22 +0000

    ATL < 700 is not supported
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1844 b35830b6-564f-0410-ac5f-b387e50cb686

--- SDK/BON/Wizard/Templates/1033/StdAfx.cpp
+++ SDK/BON/Wizard/Templates/1033/StdAfx.cpp
@@ -8,14 +8,5 @@
 
 #include <statreg.h>
 
-#if _ATL_VER < 0x0700
-#include <statreg.cpp>
-#endif // _ATL_VER < 0x0700
-
 #endif // _ATL_STATIC_REGISTRY
 
-#if _ATL_VER < 0x0700
-
-#include <atlimpl.cpp>
-
-#endif // _ATL_VER < 0x0700
