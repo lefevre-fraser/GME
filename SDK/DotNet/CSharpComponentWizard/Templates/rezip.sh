0b7bc2d26cb8e4a262dd08f29e011f17259ea47e
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2012-03-22 20:21:01 +0000

    Rezip command
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@1865 b35830b6-564f-0410-ac5f-b387e50cb686

---
+++ SDK/DotNet/CSharpComponentWizard/Templates/rezip.sh
@@ -0,0 +1,1 @@
+rm *zip &&  ( cd CSharpInterpreter && /c/Program\ Files\ \(x86\)/7-Zip/7z.exe a -x\!.svn -r ../CSharpInterpreter.zip * )  && ( cd CSharpAddon/ && /c/Program\ Files\ \(x86\)/7-Zip/7z.exe a -x\!.svn -r ../CSharpAddon.zip * )
