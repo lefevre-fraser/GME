830b4cfef2c70e343715bbfcbe61c24afa054372
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2012-09-28 12:46:30 +0000

    Need to regrelease.bat before DotNetPIAs, since tlbimp reads dependency tlb paths from the registry
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@2076 b35830b6-564f-0410-ac5f-b387e50cb686

--- Install/Build/build.py
+++ Install/Build/build.py
@@ -101,10 +101,10 @@ def compile_GME():
     tools.build_VS( sln_file, "Release" )
     sln_file = os.path.join(GME_ROOT, "GME", "GMEDecorators.sln")
     tools.build_VS( sln_file, "Release" )
-    sln_file = os.path.join(GME_ROOT, "GME", "DotNetPIAs", "DotNetPIAs.vcxproj")
-    tools.build_VS( sln_file, "Release" )
     cmd_dir = os.path.join(GME_ROOT, "GME")
     tools.system( ['call', 'regrelease.bat'] + (['x64'] if prefs['arch'] == 'x64' else []) + ['<NUL'], cmd_dir)
+    sln_file = os.path.join(GME_ROOT, "GME", "DotNetPIAs", "DotNetPIAs.vcxproj")
+    tools.build_VS( sln_file, "Release" )
 
 def _Release_PGO_dir():
     if prefs['arch'] == 'x64':
