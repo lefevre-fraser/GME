22ee4bfdb70b3360123cc4ade1a99412cc109ab2
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2014-10-29 15:43:44 +0000

    test_gmeoleapp: restore script engine
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@2558 b35830b6-564f-0410-ac5f-b387e50cb686

--- Tests/GPyUnit/test_gmeoleapp.py
+++ Tests/GPyUnit/test_gmeoleapp.py
@@ -20,32 +20,36 @@ class TestGMEOLEApp(unittest.TestCase):
         import win32com.client
 
         registrar = DispatchEx("Mga.MgaRegistrar")
+        old_engine = registrar.GetScriptEngineDisp(1)
         registrar.SetScriptEngineDisp(1, "JScript")
         
-        def _adjacent_file(file):
-            import os.path
-            return os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
-        self.gme = DispatchEx("GME.Application")
-        self.gme.Version
-        console = filter(lambda p: p.Name == "Console", self.gme.Panels)[0]
-        intf = console.Interface
-        # 'intf.Contents =' fails because python calls Invoke(wFlags=3) instead of DISPATCH_PROPERTYPUT=4
-        intf.SetContents('magicstring')
-        self.assertEqual(intf.Contents, 'magicstring')
-        
-        jscode = 'gme.ConsoleMessage("magic2string", 0);\n'
-        with open(_adjacent_file('_console_utf16.js'), 'wb') as file:
-           file.write('\xFF\xFE')
-           file.write(unicode(jscode).encode('utf-16le'))
-        intf.LoadScript(_adjacent_file('_console_utf16.js'))
-        intf.RunLoadedScript()
-        self.assertTrue(intf.Contents.find('magic2string') != -1, 'Console contents: "' + intf.Contents + '"')
-        
-        with open(_adjacent_file('_console_ansi.js'), 'wb') as file:
-            file.write(jscode.replace('magic2string', 'magic3string'))
-        intf.LoadScript(_adjacent_file('_console_ansi.js'))
-        intf.RunLoadedScript()
-        self.assertTrue(intf.Contents.find('magic3string') != -1, 'Console contents: "' + intf.Contents + '"')
+        try:
+            def _adjacent_file(file):
+                import os.path
+                return os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+            self.gme = DispatchEx("GME.Application")
+            self.gme.Version
+            console = filter(lambda p: p.Name == "Console", self.gme.Panels)[0]
+            intf = console.Interface
+            # 'intf.Contents =' fails because python calls Invoke(wFlags=3) instead of DISPATCH_PROPERTYPUT=4
+            intf.SetContents('magicstring')
+            self.assertEqual(intf.Contents, 'magicstring')
+            
+            jscode = 'gme.ConsoleMessage("magic2string", 0);\n'
+            with open(_adjacent_file('_console_utf16.js'), 'wb') as file:
+               file.write('\xFF\xFE')
+               file.write(unicode(jscode).encode('utf-16le'))
+            intf.LoadScript(_adjacent_file('_console_utf16.js'))
+            intf.RunLoadedScript()
+            self.assertTrue(intf.Contents.find('magic2string') != -1, 'Console contents: "' + intf.Contents + '"')
+            
+            with open(_adjacent_file('_console_ansi.js'), 'wb') as file:
+                file.write(jscode.replace('magic2string', 'magic3string'))
+            intf.LoadScript(_adjacent_file('_console_ansi.js'))
+            intf.RunLoadedScript()
+            self.assertTrue(intf.Contents.find('magic3string') != -1, 'Console contents: "' + intf.Contents + '"')
+        finally:
+            registrar.SetScriptEngineDisp(1, old_engine)
         
     def setUp(self):
         self.gme = None
