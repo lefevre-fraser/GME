b28a4c9c92b177dec81fc0db26705f414ed4b8b8
ksmyth <ksmyth@b35830b6-564f-0410-ac5f-b387e50cb686>
2015-11-09 19:57:03 +0000

    Target WinXP and later
    git-svn-id: https://svn.isis.vanderbilt.edu/GMESRC/trunk@2614 b35830b6-564f-0410-ac5f-b387e50cb686

--- GME/PartBrowser/targetver.h
+++ GME/PartBrowser/targetver.h
@@ -8,19 +8,19 @@
 
 // Modify the following defines if you have to target a platform prior to the ones specified below.
 // Refer to MSDN for the latest info on corresponding values for different platforms.
-#ifndef WINVER                          // Specifies that the minimum required platform is Windows 2000.
-#define WINVER 0x0500           // Change this to the appropriate value to target other versions of Windows.
+#ifndef WINVER
+#define WINVER 0x0501
 #endif
 
-#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows 2000.
-#define _WIN32_WINNT 0x0500     // Change this to the appropriate value to target other versions of Windows.
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501
 #endif
 
-#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows Me.
-#define _WIN32_WINDOWS 0x0500 // Change this to the appropriate value to target Windows Me or later.
+#ifndef _WIN32_WINDOWS
+#define _WIN32_WINDOWS 0x0501
 #endif
 
-#ifndef _WIN32_IE                       // Specifies that the minimum required platform is Internet Explorer 5.5.
-#define _WIN32_IE 0x0550        // Change this to the appropriate value to target other versions of IE.
+#ifndef _WIN32_IE
+#define _WIN32_IE 0x0550
 #endif
 
